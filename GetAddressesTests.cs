7a505bf69d0207576a16e354d5c5be6d236653e4
Giancarlo Bianchi <72824902+gc-bianchi@users.noreply.github.com>
2023-05-25 07:08:57 -0600

    Add files via upload

---
+++ GetAddressesTests.cs
@@ -0,0 +1,36 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="GetAddressesTests.cs" company="Procare Software, LLC">
+//     Copyright © 2021-2023 Procare Software, LLC. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Procare.Address.IntegrationTests;
+
+using System;
+using System.Threading.Tasks;
+
+using Xunit;
+
+public class GetAddressesTests
+{
+    private readonly AddressService service = new AddressService(new Uri("https://address.dev-procarepay.com"));
+
+    [Fact]
+    public async Task GetAddresses_With_Owm_ShouldResultIn_OneMatchingAddress()
+    {
+        var result = await this.service.GetAddressesAsync(new AddressFilter { Line1 = "1 W Main St", City = "Medford", StateCode = "OR" }).ConfigureAwait(false);
+
+        Assert.NotNull(result);
+        Assert.Equal(1, result.Count);
+        Assert.NotNull(result.Addresses);
+        Assert.Equal(result.Count, result.Addresses!.Count);
+    }
+
+    [Fact]
+    public async Task GetAddresses_With_AmbiguousAddress_ShouldResultIn_MultipleMatchingAddresses()
+    {
+        var result = await this.service.GetAddressesAsync(new AddressFilter { Line1 = "123 Main St", City = "Ontario", StateCode = "CA" }).ConfigureAwait(false);
+
+        // TODO: Complete the test
+        throw new NotImplementedException();
+    }
+}
